8743f2ff5723a841d824857233da951b02cb19ba
Chaitanya Patel <patelchaitanya514@gmail.com>
2020-10-15 02:54:36 -0400

    postman api works as expected

--- backend/server.js
+++ backend/server.js
@@ -1,68 +1,69 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const PORT = 4000; 
+const PORT = 4000;
 
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
+const Todo = require("./todo.model")
 
 
 const app = express();
 
 const todoRoutes = express.Router();//used for routingh a path in node using express. express.Router is express's method.
-app.use('/todos',todoRoutes); // we gonna use todoroutes in ouer code.
+app.use('/todos', todoRoutes); // we gonna use todoroutes in ouer code using path /todos.
 
 // usage of GET rerquest in router
-todoRoutes.route('/').get(function(req,res){
-    Todo.find(function(err,todos){
-      if(err){
-          console.log("error",err);
-      }else{
-          res.json(todos)
-      }
+todoRoutes.route('/').get(function (req, res) {
+    Todo.find(function (err, todos) {
+        if (err) {
+            console.log("error", err);
+        } else {
+            res.json(todos)
+        }
     });
 });
 
 //we will be getting data from mongoDB database 
 // findById is the method that we can retrive from express.
-todoRoutes.route("/:id").get(function(req,res){
+todoRoutes.route("/:id").get(function (req, res) {
     let id = req.params.id;
-    Todo.findById(id,function(err,todo){
+    Todo.findById(id, function (err, todo) {
         res.json(todo)
     });
 });
 
 // Now we will be adding new data into database by using HTTP's POST request.
 
-todoRoutes.route("/add").post(function(req,res){
+todoRoutes.route("/add").post(function (req, res) {
     let todo = new Todo(req.body);
     todo.save()
-    .then(todo=>{
-        res.status(200).json({'todo':'todo added successfully'});
-    })
-    .catch(err=>{
-        res.status(400).send('addingf new todo failed')
-    });
+        .then(todo => {
+            res.status(200).json({ 'todo': 'todo added successfully' });
+        })
+        .catch(err => {
+            res.status(400).send('addingf new todo failed')
+        });
 });
 
 //todo update routes
 
-todoRoutes.route("/update/:id").post(function(req,res){
-    Todo.findById(req.params.id,function(err,todo){
-        if(!todo){
-            res.status(404).send("data is not found"); 
-        }else{
-            todo.todo_description=req.body.todo_description;
+todoRoutes.route("/update/:id").post(function (req, res) {
+    Todo.findById(req.params.id, function (err, todo) {
+        if (!todo) {
+            res.status(404).send("data is not found");
+        } else {
+            todo.todo_description = req.body.todo_description;
             todo.todo_responsible = req.body.todo_responsible;
             todo.todo_priority = req.body.todo_priority;
             todo.todo_completed = req.body.todo_completed;
 
-            todo.save().then(todo=>{
+            todo.save().then(todo => {
                 res.json('Todo updated');
             })
 
-            .catch(err=>{
-                res.status(400).send("update not possible");
-            });
+                .catch(err => {
+                    res.status(400).send("update not possible");
+                });
 
         }
     });
@@ -71,13 +72,13 @@ todoRoutes.route("/update/:id").post(function(req,res){
 app.use(cors());
 app.use(bodyParser.json);
 
-mongoose.connect('mongodb://127.0.0.1:27017/todos',{useNewUrlParser:true});
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
 const connection = mongoose.connection;
 
-connection.once('open', function(){
+connection.once('open', function () {
     console.log("mongodb connection established successfully");
 })
 
-app.listen(PORT, function(){
-    console.log("server is running on ",+ PORT);
+app.listen(PORT, function () {
+    console.log("server is running on ", + PORT);
 });
