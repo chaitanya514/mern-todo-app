c3ec205906a00af0d6304ddf841aca02391b623f
Chaitanya Patel <patelchaitanya514@gmail.com>
2020-10-16 02:40:08 -0400

    final code done App is up and running

--- src/todos-list.component.js
+++ src/todos-list.component.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
-
+import "./todocompleted.css"
 
 const Todo = (props) => {
  return (
   <tr>
-    <td>{props.todo.todo_description}</td>
-    <td>{props.todo.todo_responsible}</td>
-    <td>{props.todo.todo_priority}</td>
+    <td className={props.todo.todo_completed?'completed':''}>{props.todo.todo_description}</td>
+    <td className={props.todo.todo_completed?'completed':''}>{props.todo.todo_responsible}</td>
+    <td className={props.todo.todo_completed?'completed':''}>{props.todo.todo_priority}</td>
     <td>
       <Link to={"/edit/" + props.todo._id}> Edit</Link>
     </td>
